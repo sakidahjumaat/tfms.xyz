f136aa98e03d8abcc77ebab6f00755fa55e83c63
sakidahjumaat <jemremy82@gmail.com>
2025-12-11 12:40:07 +0800

    Update daddylive-channels-epg-grabber.py

--- epgs/daddylive-channels-epg-grabber.py
+++ epgs/daddylive-channels-epg-grabber.py
@@ -115,6 +115,7 @@ urls = [
     "https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_UY1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_ZA1.xml.gz",
+    "https://www.open-epg.com/files/unitedstates1.xml.gz";
 ]
 
 if __name__ == "__main__":
